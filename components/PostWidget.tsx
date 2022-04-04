7384f75c10200ad479f1da5f3e0a8a76810d2de2
Alex Padilla <alex.padilla@epn.edu.ec.com>
2022-04-04 15:56:46 -0500

    Add Widget Component

--- components/PostWidget.tsx
+++ components/PostWidget.tsx
@@ -1,8 +1,55 @@
+import { useEffect, useState } from 'react'
+import moment from 'moment'
+import Link from 'next/link'
 
+import { getRecentPosts, getSimilarPosts } from '@services/index'
+import { PostType } from 'types'
 
-const PostWidget: React.FC= () => {
+const PostWidget: React.FC<{ categories: string; slug: string }> = ({
+  categories,
+  slug,
+}) => {
+  const [relatedPosts, setRelatedPosts] = useState<PostType[]>([])
+
+  useEffect(() => {
+    if (slug) {
+      getSimilarPosts(categories, slug).then((result) =>
+        setRelatedPosts(result)
+      )
+    } else {
+      getRecentPosts().then((result) => setRelatedPosts(result))
+    }
+  }, [slug])
+
+  console.log(relatedPosts)
+
   return (
-    <div>PostWidget</div>
+    <div className="mb-8 rounded-lg bg-white p-8 shadow-lg">
+      <h3 className="mb-8 border-b pb-4 text-xl font-semibold ">
+        {slug ? 'Related Posts' : 'Recent Posts'}
+      </h3>
+      {relatedPosts.map((post) => (
+        <div key={post.title} className="flex w-full items-center">
+          <div className="w-16 flex-none">
+            <img
+              src={post.featuredImage.url}
+              alt={post.title}
+              width="60px"
+              height="60px"
+              className="rounded-full align-middle"
+            />
+          </div>
+          <div className="ml-4 flex-grow">
+            <p className="text-gray-500 font-xs">
+              {moment(post.createdAt).format('MMM, D, YYYY')}
+            </p>
+            <Link href={`/posts/${post.slug}`} key={post.title} >
+              {post.title}
+            </Link>
+          </div>
+        </div>
+      ))}
+    </div>
   )
 }
 
